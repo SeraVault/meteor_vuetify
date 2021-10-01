8f75976ed92dc1603d22fee92b1b270e35403ea9
Donnie Guedry <dguedry@gmail.com>
2021-10-01 17:23:39 -0500

    Fix automatic translation

--- exec/api/translate.js
+++ exec/api/translate.js
@@ -1,17 +1,43 @@
 /* use Google Cloud Translate service to translate from your defaultLocale defined in your settings.json to all other languages defined in settings (i18n settings) */
-
-const { exec }  = require('child_process')
+var TJO = require('translate-json-object')();
+//const { exec }  = require('child_process')
+var fs = require('fs')
 
 var settings = require("../../settings.json")
 var from = settings.public.i18n.defaultLocale
 var to = []
 var apiKey = settings.private.i18n.googleTranslateApiKey
+
 settings.public.i18n.languages.forEach(lang => {
   if (lang.value != from) {
     to.push(lang.value)
   }
 })
 
+TJO.init({
+  googleApiKey: apiKey
+});
+
+const rd = process.env.PWD;
+
+const source = JSON.parse(fs.readFileSync(`${rd}/public/locales/${from}.json`))
+
+to.forEach(lang => {
+  console.log(lang)
+  TJO.translate(source, lang)
+    .then(function (data) {
+      console.log(data)
+      fs.writeFile(`${rd}/public/locales/${lang}.json`, JSON.stringify(data, null, 2), (err) => {
+        if (err) throw err;
+        console.log(`${lang}.json has been saved`);
+      });
+    })
+    .catch(function (err) {
+      console.log('error ', err)
+    });
+})
+
+/*
 var command = `i18n-translate-json ${apiKey} public/locales/ ${from} ${to}`
 if (settings.public.authentication.allowMeteor) {
   command += ` ; i18n-translate-json ${apiKey} imports/startup/server/auth/meteor-accounts/locales/ ${from} ${to}`
@@ -33,4 +59,4 @@ var ex = exec(command, (error, stdout, stderr) => {
 ex.stdout.on('data', function(data) {
   console.log(data); 
 });
-
+*/
